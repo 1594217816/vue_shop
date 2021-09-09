c0bb22ef726abee7c0e8211332ae75409b6ad3d7
itcast <1594217816@qq.com>
2021-09-09 14:24:15 +0800

    登录功能提交

---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><el-button type="info" round @click="loginOut">退出登录</el-button></div>
+</template>
+<script>
+export default {
+  methods: {
+    //  退出功能
+    loginOut() {
+      // 删除token
+      window.sessionStorage.removeItem('token');
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
+<style>
+</style>
